95280b4e28ea8b3d2f480965f2c68adcbc64c7a5
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2025-05-18 16:30:57 +0300

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,10 @@
+﻿window.getWindowWidth = () => {
+    return window.innerWidth;
+};
+
+window.registerResizeCallback = (dotNetHelper) => {
+    window.addEventListener("resize", () => {
+        dotNetHelper.invokeMethodAsync("OnResize", window.innerWidth);
+    });
+};
+
